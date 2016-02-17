a70a8c6a6c1f0b25242c27ebb17321d975295d14
rathodn <rathodnileshn@gmail.com>
2016-02-17 04:27:50 -0700

    cleaning first; added features

--- Exploratory Analysis Rossmann.R
+++ Exploratory Analysis Rossmann.R
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 library(data.table)
 library(zoo)
 
@@ -31,9 +31,9 @@ summary(test)
 train[, lapply(.SD, function(x) length(unique(x)))]
 test[, lapply(.SD, function(x) length(unique(x)))]
 
-train[unique(train$StateHoliday)]
 
-sha# All test stores are also in the train data
+
+# All test stores are also in the train data
 sum(unique(test$Store) %in% unique(train$Store)) 
 
 # All the train stores are in the test data
@@ -171,7 +171,6 @@ store$Promo2Since <- as.POSIXct(paste(store$Promo2SinceYear,
                                 format = "%Y-%U-%u")
 hist(as.numeric(as.POSIXct("2015-10-01", format = "%Y-%m-%d") - store$Promo2Since), 
      100, main = "Days since start of promo2")
-
 
 table(store$PromoInterval)
 
@@ -320,3 +319,88 @@ write.csv(mdl1_gm_predictions, "mdl1_predictions.csv",row.names=F)
 ####11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
 
 ## Cleaning Data
+str(train_store)
+
+#In stores data we have Three stores with Store numbers 81, 862 and 957 has competition distance == NA
+sum(is.na(store$CompetitionDistance))
+store[is.na(CompetitionDistance)]
+
+#in stores data, But 317 Stores have NA for competition starting information i.e. month, yr, week
+sum(is.na(store$CompetitionOpenSinceYear))
+
+# Thats why in train data we have CompetitionDistance NA's are only 2516;
+#although CompetitionOpenSince NA's are 276100; so there are lot of missing values.
+sum(is.na(train_store$CompetitionDistance))
+sum(is.na(train_store$CompetitionOpenSinceYear))
+
+sum(is.na(train_store$CompetitionDistance))
+sum(is.na(train_store$CompetitionOpenSinceYear))
+
+train[Sales != 0 & DayOfWeek==7]
+store[is.na(CompetitionDistance)]
+
+# In these 861 cases the 143 unique stores were open even though it was stateholiday; not error but exceptional stores
+# We will add column named exceptional store if the store is open in case of stateholiday to train and test data 
+openstores_on_stateHolidays = train_store[Open == 1 & StateHoliday != 0]
+unique(openstores_on_stateHolidays$Store)
+
+
+# For 166,190 cases the store is not continuing with promo2 eventhough they ran promo1
+train_store[Promo == 1 & Promo2==0]
+
+
+#work on f_train_store for adding features and
+f_train_store = train_store
+
+
+#Added How Many days since promo started column: promo2Days
+
+#convert promo2Since to date format
+f_train_store$Promo2Since = as.Date(f_train_store$Promo2Since)
+
+#Added promo2Days column = diff of days bet current store date - promo started date 
+# Negative value means how many days after that day the promo will start
+f_train_store$promo2Days = f_train_store$Date - f_train_store$Promo2Since
+
+
+#Promo Interval - Each round started month; so we can add column of promo2 round Number x started and 
+#watch effect / drop in sales after how many days
+
+
+## as given all schools are closed on public holidays and weekends (weekends == Sundays)
+## trying to identify cases where schools are off and has no effect on sales
+# Adding column if schoolOff 1 means off and 0 means running (no off)
+f_train_store$schoolOff[f_train_store$schoolOff] = 0
+f_train_store$schoolOff[f_train_store$StateHoliday == "a" | f_train_store$DayOfWeek == 7 | f_train_store$SchoolHoliday == 1] = 1
+
+#Found 1,34,583 cases where store sales not affected even though schools are off (121350 (weekends off) + 13505(public holiday off) - 272 (both))
+nrow(f_train_store[SchoolHoliday == 0 & schoolOff==1])
+
+f_train_store[f_train_store$SchoolHoliday == 1]
+#150963
+
+nrow(f_train_store[SchoolHoliday == 1 & StateHoliday != 0])
+# 2438 weekend
+# 4663 public holiday A
+# 13889 state holiday
+
+#= values[match()]
+
+
+dd$seg[dd$x2> 0 && dd$x3> 200] <-1
+
+
+
+
+
+f_train_store$schoolOff  <- sapply(f_train_store,isSchoolOff)
+
+str(train_store)
+head(train_store)
+
+train_store[is.na(train_store$schoolOff)]
+train_store[is.na(schoolOff)]
+any(is.na(train_store$schoolOff))
+
+train_store[, lapply(.SD, function(x) length(unique(x)))]
+head(train_store)
