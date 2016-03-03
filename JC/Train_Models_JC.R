b56867c74f4ffeadfa9178bedc6118c80c199d8f
Cotrell <jcotrell@nrel.gov>
2016-03-03 15:38:28 -0700

    Created RF2 model and wrote predictions to CSV

--- JC/Train_Models_JC.R
+++ JC/Train_Models_JC.R
@@ -32,7 +32,7 @@
     
    #Fitting a linear model*********************************************
      
-     lm1.model = lm(Sales ~ DayOfWeek + StateHoliday + Assortment + Customers + StoreType, data = train_d.n0)
+     lm1.model = lm(Sales ~ DayOfWeek + StateHoliday + Assortment + StoreType, data = train_d.n0)
        #Notes 
           #I had an error when trying to use "Open" so I removed it
           #CompetitionDistance has NA values so I removedDayOfWeek 
@@ -44,27 +44,41 @@
          lm1.pred <- predict(lm1.model, newdata = test_d.n0)
          head(lm1.pred); str(lm1.pred)
          qplot(lm1.pred)
-         RMSPE.lm1 =  sqrt( (sum( (test_d.n0$Sales - lm1.pred)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.lm1 #26.98 
+         RMSPE.lm1 =  sqrt( (sum( (test_d.n0$Sales - lm1.pred)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.lm1 #111% 
      
  #Fitting a random forest tree********************************* 
          library(randomForest)
          
          #mtry=1
-         rf.model = randomForest(Sales ~ DayOfWeek + StateHoliday + Assortment + Customers + StoreType, data = train_d.n0, mtry=1, ntree=5)
+         rf.model = randomForest(Sales ~ DayOfWeek + StateHoliday + Assortment  + StoreType, data = train_d.n0, mtry=1, ntree=5)
          print(rf.model)
          importance(rf.model)
          
          rf.model.pred = predict(rf.model, newdata=test_d.n0)
          head(rf.model.pred); str(rf.model.pred)
          qplot(rf.model.pred)
-         RMSPE.rf =  sqrt( (sum( (test_d.n0$Sales - rf.model.pred)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.rf #68
+         RMSPE.rf =  sqrt( (sum( (test_d.n0$Sales - rf.model.pred)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.rf #117%
          
          #mtry=2 (Bagging)
-         rf.model2 = randomForest(Sales ~ DayOfWeek + StateHoliday + Assortment + Customers + StoreType, data = train_d.n0, mtry=2, ntree=5)
+         rf.model2 = randomForest(Sales ~ DayOfWeek +  Assortment  + StoreType, data = train_d.n0, mtry=2, ntree=25)
+         #removed state holiday due to factors of different levels in test.merged.
          print(rf.model2)
          importance(rf.model2)
+         
          
          rf.pred2 = predict(rf.model2, newdata=test_d.n0)
          head(rf.pred2); str(rf.pred2)
          qplot(rf.pred2)
-         RMSPE.rf2 =  sqrt( (sum( (test_d.n0$Sales - rf.pred2)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.rf2 #31%
+         RMSPE.rf2 =  sqrt( (sum( (test_d.n0$Sales - rf.pred2)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.rf2 #105%
+         
+         #mtry=2 (Bagging with all variables)
+         rf.model3 = randomForest(Sales ~ DayOfWeek + StateHoliday + Promo + Assortment  + StoreType + SchoolHoliday, data = train_d.n0, mtry=2, ntree=25)
+         #Competition Distance is removed due to missing values
+         print(rf.model3)
+         importance(rf.model3)
+         
+         rf.pred3 = predict(rf.model3, newdata=test_d.n0)
+         head(rf.pred3); str(rf.pred3)
+         qplot(rf.pred3)
+         RMSPE.r32 =  sqrt( (sum( (test_d.n0$Sales - rf.pred3)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.rf3 #106%
+         
