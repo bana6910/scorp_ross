0345e48abe7ccea7d477c859bac6be61cd22fee7
Cotrell <jcotrell@nrel.gov>
2016-03-09 13:31:08 -0700

    Updated training models and prediction script

--- 4_Train_Models_JC.R
+++ 4_Train_Models_JC.R
@@ -16,27 +16,44 @@
     library(Cubist)
 
   #Check Training Data
-    head(train.merged)
-    tail(train.merged)
-    str(train.merged)
-    dim(train.merged)
-    summary(train.merged)
-    colSums(is.na(train.merged))  #Check for NA (misssumming) value
+    # head(train.merged)
+    # tail(train.merged)
+    # str(train.merged)
+    # dim(train.merged)
+    # summary(train.merged)
+    # colSums(is.na(train.merged))  #Check for NA (misssumming) value
 
 
+ #Break training data in 1/2 for training models and testing models (this should be changed to 75%/25%) 
+    set.seed(123) #set a seed to provide some repeatability when sampling
+    train_ind = sample(nrow(f_train_store), nrow(f_train_store)/2) #creates a training index vector of size nrow/2.
+    train_d = f_train_store[train_ind,]#create the training and test data sets by pulling all indexed rows from d 
+    test_d = f_train_store[-train_ind,]
+    #Remove zeros from test_d consistent with test data provided by Daria
+    test_d.n0 = test_d[test_d$Sales != 0,]
+    summary(test_d.n0)
+    #Repeat for training data for convenience later
+    train_d.n0 = train_d[train_d$Sales != 0,]
+    summary(train_d.n0)
+    #Repeat for all training data for convenience later
+    f_train_store.n0 = f_train_store[f_train_store$Sales != 0,]
+    summary(train_d.n0)
+    
+    
   #Predictions
   
     #Compute mean of sales as a baseline prediction with zeros removed**************************
       mean.model <- mean(train_d.n0$Sales); mean.model #7070
-      RMSPE.mean =  sqrt( (sum( (test_d.n0$Sales - mean.model)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.mean # Compute Root Mean Square Percentage Error (RMSPE)
+      RMSPE.mean =  sqrt( (sum( (test_d.n0$Sales - mean.model)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.mean # Compute Root Mean Square Percentage Error (RMSPE) 121
     
    #Fitting a linear model*********************************************
      
-     lm1.model = lm(Sales ~ DayOfWeek + StateHoliday + Assortment + StoreType, data = train_d.n0)
+     lm1.model = lm(Sales ~ DayOfWeek + StateHoliday + Assortment + StoreType + Promo + CompetitionDistance + CompetitionOpenSince + promo2Days, data = train_d.n0)
        #Notes 
           #I had an error when trying to use "Open" so I removed it
-          #CompetitionDistance has NA values so I removedDayOfWeek 
           #Removed school holiday due to high P value
+          #Note Promo2Days is not valuable 
+          #CompetitionOPenSince is not valuable
      
       summary(lm1.model) #State holiday b and C aren't significant, but leave them for now as removing doesn't help Adj R square .7954
      
@@ -44,23 +61,14 @@
          lm1.pred <- predict(lm1.model, newdata = test_d.n0)
          head(lm1.pred); str(lm1.pred)
          qplot(lm1.pred)
-         RMSPE.lm1 =  sqrt( (sum( (test_d.n0$Sales - lm1.pred)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.lm1 #111% 
+         RMSPE.lm1 =  sqrt( (sum( (test_d.n0$Sales - lm1.pred)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.lm1 #92% 
      
  #Fitting a random forest tree********************************* 
          library(randomForest)
          
-         #mtry=1
-         rf.model = randomForest(Sales ~ DayOfWeek + StateHoliday + Assortment  + StoreType, data = train_d.n0, mtry=1, ntree=5)
-         print(rf.model)
-         importance(rf.model)
-         
-         rf.model.pred = predict(rf.model, newdata=test_d.n0)
-         head(rf.model.pred); str(rf.model.pred)
-         qplot(rf.model.pred)
-         RMSPE.rf =  sqrt( (sum( (test_d.n0$Sales - rf.model.pred)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.rf #117%
-         
+        
          #mtry=2 (Bagging)
-         rf.model2 = randomForest(Sales ~ DayOfWeek +  Assortment  + StoreType, data = train_d.n0, mtry=2, ntree=25)
+         rf.model2 = randomForest(Sales ~ DayOfWeek + Assortment + StoreType + Promo + promo2Days, data = train_d.n0, mtry=2, ntree=5)
          #removed state holiday due to factors of different levels in test.merged.
          print(rf.model2)
          importance(rf.model2)
@@ -69,16 +77,26 @@
          rf.pred2 = predict(rf.model2, newdata=test_d.n0)
          head(rf.pred2); str(rf.pred2)
          qplot(rf.pred2)
-         RMSPE.rf2 =  sqrt( (sum( (test_d.n0$Sales - rf.pred2)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.rf2 #105%
+         RMSPE.rf2 =  sqrt( (sum( (test_d.n0$Sales - rf.pred2)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.rf2 #88%
          
          #mtry=2 (Bagging with all variables)
-         rf.model3 = randomForest(Sales ~ DayOfWeek + StateHoliday + Promo + Assortment  + StoreType + SchoolHoliday, data = train_d.n0, mtry=2, ntree=25)
-         #Competition Distance is removed due to missing values
+         rf.model3 = randomForest(Sales ~ DayOfWeek + StateHoliday + Promo + Assortment  + StoreType + SchoolHoliday + promo2Days + CompetitionDistance, data = train_d.n0, mtry=2, ntree=5)
          print(rf.model3)
          importance(rf.model3)
          
          rf.pred3 = predict(rf.model3, newdata=test_d.n0)
          head(rf.pred3); str(rf.pred3)
          qplot(rf.pred3)
-         RMSPE.r32 =  sqrt( (sum( (test_d.n0$Sales - rf.pred3)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.rf3 #106%
+         RMSPE.rf3 =  sqrt( (sum( (test_d.n0$Sales - rf.pred3)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.rf3 #81%
+         
+         #Remove SchoolHoliday & StateHoliday
+         rf.model3 = randomForest(Sales ~ DayOfWeek +  Promo + Assortment  + StoreType +  promo2Days + CompetitionDistance, data = train_d.n0, mtry=2, ntree=5)
+         #note promo2Days is making this run slow
+         print(rf.model3)
+         importance(rf.model3)
+         
+         rf.pred3 = predict(rf.model3, newdata=test_d.n0)
+         head(rf.pred3); str(rf.pred3)
+         qplot(rf.pred3)
+         RMSPE.rf3 =  sqrt( (sum( (test_d.n0$Sales - rf.pred3)/test_d.n0$Sales )^2 ) / nrow(test_d.n0) ); RMSPE.rf3 #74%
          
